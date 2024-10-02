b1cab2b3bd7302febbab31a82c80030cf532cf0a
Johnny Proano <jproano22@yahoo.com>
2024-10-02 17:25:52 -0400

    Added query text field and button

--- CitySights App/ContentView.swift
+++ CitySights App/ContentView.swift
@@ -8,12 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var query: String = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        HStack {
+            
+            TextField("Waht are you looking for?", text: $query)
+                .textFieldStyle(.roundedBorder)
+            
+            Button {
+//                To do
+            } label: {
+                Text("Go")
+            }
+
         }
         .padding()
     }
