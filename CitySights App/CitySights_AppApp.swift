d0aee3dc9ec39738d0af22dbf4adac5005682e7c
Johnny Proano <jproano22@yahoo.com>
2024-10-02 11:42:13 -0400

    Initial Commit

---
+++ CitySights App/CitySights_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CitySights_AppApp.swift
+//  CitySights App
+//
+//  Created by Johnny Proano on 10/2/24.
+//
+
+import SwiftUI
+
+@main
+struct CitySights_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
