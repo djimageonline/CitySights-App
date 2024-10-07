489add88e94338c83c8ffe51334aff6e39269162
Johnny Proano <jproano22@yahoo.com>
2024-10-07 08:47:42 -0400

    added api calls and headers

--- CitySights App/DataService.swift
+++ CitySights App/DataService.swift
@@ -6,3 +6,36 @@
 //
 
 import Foundation
+
+struct DataService {
+    let apiKey = Bundle.main.infoDictionary?["API_KEY"] as? String
+
+    func businessSearch() async {
+        
+
+        //    Check id api exists
+        guard apiKey != nil else {
+            return
+        }
+        //    create url
+        if let url = URL(string: "https://api.yelp.com/v3/businesses/search?latitude=35.665517&longitude=139.770398&categories=restaurants&limit=10") {
+            
+            //    create request
+            var request = URLRequest(url: url)
+            request.addValue("Bearer \(apiKey!)", forHTTPHeaderField: "Authorization")
+            request.addValue("application/json", forHTTPHeaderField: "accept")
+            
+            //    sendrequest
+            
+            do {
+                let (data, response) =  try await URLSession.shared.data(for: request)
+                
+                print (data)
+                print (response)
+            }
+            catch {
+                print(error)
+            }
+        }
+    }
+}
